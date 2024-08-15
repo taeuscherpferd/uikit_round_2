10f74621c4bb313f12467d92370f82edde44265b
taeuscherpferd <ibber@myokcorral.com>
2024-08-14 22:36:57 -0600

    Added defaults and optimizations

--- src/hooks/useLocomotion.ts
+++ src/hooks/useLocomotion.ts
@@ -1,90 +1,91 @@
 import { useFrame, useThree } from "@react-three/fiber";
 import { useXRControllerState } from "@react-three/xr";
-import { useEffect, useRef } from "react";
-import { BufferGeometry, Group, Line, LineBasicMaterial, MathUtils, Quaternion, Vector3 } from "three";
+import { useRef } from "react";
+import { Group, MathUtils, Quaternion, Vector3 } from "three";
 
 export interface LocomotionOptions {
-  handControllingMovement: 'left' | 'right'
+  handControllingMovement?: 'left' | 'right'
+  speed?: number
+  numberOfDegreesToSnapTurnBy?: number
+  snapTurningDeadZone?: number
+  disableSnapTurning?: boolean
 }
 
-const defaultSpeed = 60
-const thumbstickPropName = 'xr-standard-thumbstick'
-const geometry = new BufferGeometry();
-const material = new LineBasicMaterial({ color: 0xff0000, linewidth: 3 }); // Red color
-const line = new Line(geometry, material);
-const cameraQuaternion = new Quaternion()
+const defaultSpeed = 1;
+const defaultNumberOfDegreesToSnapTurnBy = 45;
+const defaultHandControllingMovement = 'left';
+const defaultSnapTurningDeadZone = 0.5;
+const thumbstickPropName = 'xr-standard-thumbstick';
+const cameraQuaternion = new Quaternion();
 
 export const useLocomotion = (options: LocomotionOptions) => {
-  const { handControllingMovement } = options;
+  const { handControllingMovement = defaultHandControllingMovement, speed = defaultSpeed, numberOfDegreesToSnapTurnBy = defaultNumberOfDegreesToSnapTurnBy,
+    snapTurningDeadZone = defaultSnapTurningDeadZone, disableSnapTurning } = options;
+
   const positionInfo = useRef<Group>(null);
   const canRotate = useRef(true);
-  const camera = useThree((s) => s.camera)
-  const scene = useThree((s) => s.scene)
+  const camera = useThree((s) => s.camera);
 
   const l_controller = useXRControllerState("left");
   const r_controller = useXRControllerState("right");
   const movementController = handControllingMovement === 'left' ? l_controller : r_controller;
   const viewController = handControllingMovement === 'left' ? r_controller : l_controller;
 
-  useEffect(() => {
-    scene.add(line)
-    return () => {
-      scene.remove(line)
-    }
-  });
-
   useFrame((_, delta) => {
     if (positionInfo.current == null || movementController == null || viewController == null) return;
 
     const movementThumbstickState = movementController.gamepad[thumbstickPropName];
+
     const movementXAxisOrDefault = movementThumbstickState?.xAxis ?? 0;
     const movementYAxisOrDefault = movementThumbstickState?.yAxis ?? 0;
 
     const viewThumbstickState = viewController.gamepad[thumbstickPropName];
     const viewXAxisOrDefault = viewThumbstickState?.xAxis ?? 0;
 
-    if (movementThumbstickState == null) return
+    // If no joystick input, return
+    if (movementXAxisOrDefault === 0 && movementYAxisOrDefault === 0 && viewXAxisOrDefault === 0) return
 
     // Handle snapping rotation using the viewController
-    let rotationQuaternion = null
-    if (viewXAxisOrDefault < -0.5 && canRotate.current) {
-      canRotate.current = false
-      rotationQuaternion = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), MathUtils.degToRad(45));
-      positionInfo.current.quaternion.multiply(rotationQuaternion);
-    }
-    else if (viewXAxisOrDefault > 0.5 && canRotate.current) {
-      canRotate.current = false
-      rotationQuaternion = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), -MathUtils.degToRad(45));
-      positionInfo.current.quaternion.multiply(rotationQuaternion);
-    }
-    else if (viewXAxisOrDefault > -0.5 && viewXAxisOrDefault < 0.5) {
-      canRotate.current = true
+    let rotationQuaternion = null;
+    if (!disableSnapTurning) {
+      if (viewXAxisOrDefault < -snapTurningDeadZone && canRotate.current) {
+        canRotate.current = false;
+        rotationQuaternion = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), MathUtils.degToRad(numberOfDegreesToSnapTurnBy));
+        positionInfo.current.quaternion.multiply(rotationQuaternion);
+      }
+      else if (viewXAxisOrDefault > snapTurningDeadZone && canRotate.current) {
+        canRotate.current = false;
+        rotationQuaternion = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), -MathUtils.degToRad(numberOfDegreesToSnapTurnBy));
+        positionInfo.current.quaternion.multiply(rotationQuaternion);
+      }
+      else if (viewXAxisOrDefault > -snapTurningDeadZone && viewXAxisOrDefault < snapTurningDeadZone) {
+        canRotate.current = true;
+      }
     }
 
+    // Handle movement using the movementController
     const inputVector = new Vector3(movementXAxisOrDefault, 0, movementYAxisOrDefault)
-    camera.getWorldQuaternion(cameraQuaternion)
-
-    inputVector.applyQuaternion(cameraQuaternion)
+    camera.getWorldQuaternion(cameraQuaternion);
+    inputVector.applyQuaternion(cameraQuaternion);
 
     if (rotationQuaternion) {
-      inputVector.applyQuaternion(rotationQuaternion)
+      inputVector.applyQuaternion(rotationQuaternion);
     }
 
-
-    let xChange = positionInfo.current.position.x
-    let zChange = positionInfo.current.position.z
+    let xChange = positionInfo.current.position.x;
+    let zChange = positionInfo.current.position.z;
 
     if (inputVector.x !== 0) {
-      xChange += (inputVector.x) * delta;
+      xChange += (inputVector.x) * delta * speed;
     }
 
     if (inputVector.z !== 0) {
-      zChange += (inputVector.z) * delta
+      zChange += (inputVector.z) * delta * speed;
     }
 
     if (xChange !== positionInfo.current.position.x || zChange !== positionInfo.current.position.z) {
-      positionInfo.current.position.x = xChange
-      positionInfo.current.position.z = zChange
+      positionInfo.current.position.x = xChange;
+      positionInfo.current.position.z = zChange;
     }
   })
 
